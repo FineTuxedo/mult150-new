67b316b7585e28e7a58a673938979e411925f0d5
FineTuxedo <artistadamsv@gmail.com>
2026-05-31 22:31:25 -0700

    new

--- asvoboda_Hour20/Assets/Scenes/scripts/GameCritic.cs
+++ asvoboda_Hour20/Assets/Scenes/scripts/GameCritic.cs
@@ -2,4 +2,8 @@
 2 To make it easier i would make feedback more intuitive and make a loading screen of sorts that would explain the point. to make it harder i would simple extend the time of the game.
 3 a more clear objective would be very beneficcial to appeal. 
 4 i liked running and jumping. 
-5 none (its very straight forward in a sense. i think obly a title page telling me what to do would help*/
+5 none (its very straight forward in a sense. i think obly a title page telling me what to do would help
+
+made a fence obstacle, gave it a wood texture
+made a trophy powerup, changed the texture, made a new particle
+*/
